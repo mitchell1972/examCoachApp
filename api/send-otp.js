f3e242117dd626925d9d0244af3d70bd2633fe10
mitchellagoma <mitchellagoma@gmail.com>
2025-07-28 17:11:06 +0100

    fix: change cors origin to wildcard for testing

--- api/send-otp.js
+++ api/send-otp.js
@@ -6,7 +6,7 @@ const client = twilio(
 );
 
 module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://mitchell1972.github.io');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
